8d39f0dfcbe09e19958a0e7321475d9cdc91e548
preston-tech <prestonphillips42@gmail.com>
2020-07-23 10:01:39 -0600

    created manifest file to use this as a chrome extenstion

--- video.js
+++ video.js
@@ -5,3 +5,24 @@ document.querySelector("video");
 video.playbackRate = 0.5;
 video.playbackRate = 2;
 video.playbackRate = 1;
+
+DocumentTimeline.addEventListener("keydown", e => {
+    checkLetter(e.key)
+})
+
+function checkLetter(key) {
+    switch(key) {
+        case "a":
+            video.playbackRate += 0.1;
+            console.log(`current Rate: ${video.playbackRate.toFixed(2)}`)
+            break;        
+        case "s":
+            video.playbackRate -= 0.1;
+            console.log(`current Rate: ${video.playbackRate.toFixed(2)}`)
+            break;        
+        case "d":
+            video.playbackRate = 0.1;
+            console.log(`current Rate: ${video.playbackRate.toFixed(2)}`)
+            break;
+    }
+}
